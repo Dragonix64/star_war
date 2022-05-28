a5688a2257de22e5fd56044e6ac443eb5a2e9a0a
Dragonix64 <105961713+Dragonix64@users.noreply.github.com>
2022-05-28 10:49:05 +0600

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -1,5 +1,13 @@
+from pygame import *
+from random import randint
 
-from pygame import*
+font.init()
+font1 = font.Font(None, 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+
+font2 = font.Font(None, 36)
+
 
 mixer.init()
 mixer.music.load('space.ogg')
@@ -7,102 +15,142 @@ mixer.music.play()
 fire_sound = mixer.Sound('fire.ogg')
 
 
-init()
-window = display.set_mode((500, 700))
-display.set_caption("Стрелялка")
+img_back = "galaxy.jpg" 
+ 
+img_bullet = "bullet.png"
+img_hero = "rocket.png" 
+img_enemy = "ufo.png" 
+ 
+score = 0 
+goal = 10 
+lost = 0 
+max_lost = 3
+ 
 
 class GameSprite(sprite.Sprite):
+
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+
         sprite.Sprite.__init__(self)
+
+
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
+
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+ 
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-
-
-
 
 
 class Player(GameSprite):
+
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > - 60:
+        if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 250:
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
+
     def fire(self):
-        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -10)
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
         bullets.add(bullet)
-            
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-    
-class Enemy(sprite.Sprite):
+
+
+class Enemy(GameSprite):
+
     def update(self):
         self.rect.y += self.speed
         global lost
+
         if self.rect.y > win_height:
-            self.rect.x = randit(80, win_widtg - 80)
+            self.rect.x = randint(80, win_width - 80)
             self.rect.y = 0
             lost = lost + 1
-
+ 
+ 
 class Bullet(GameSprite):
+
     def update(self):
         self.rect.y += self.speed
+
         if self.rect.y < 0:
             self.kill()
+ 
 
-
-
-display.set_caption("Maze")
-game = True
-finish = False
-clock = time.Clock()
-FPS = 60
-background = transform.scale(image.load('galaxy.jpg'), (500, 700))
 win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+ 
 
-player = Player('rocket.png', 200, 550, 80, 100, 30)
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+ 
+
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+    monsters.add(monster)
+ 
 bullets = sprite.Group()
-game = True
-while game is True:
-    for ev in event.get():
-        if ev.type == QUIT:
-            game = False
+ 
 
-        elif ev.type == KEYDOWN:
-            if ev.key == K_SPACE:
+finish = False
+
+run = True
+while run:
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
                 fire_sound.play()
-                player.fire()
-        
+                ship.fire()
+ 
+
+    if not finish:
+
+        window.blit(background,(0,0))
 
 
+        text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
 
-    if finish != True:
-        window.blit(background,(0, 0))
-        player.reset()
-        player.update()
-        text = font2.render('Счет: ' + str(score), 1, (255, 255, 255))
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
         window.blit(text_lose, (10, 50))
+
+
+        ship.update()
         monsters.update()
-
-
-
-
-
-        monsters.draw(window)
         bullets.update()
+
+        ship.reset()
+        monsters.draw(window)
         bullets.draw(window)
+ 
+
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        for c in collides:
+
+            score = score + 1
+            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+            monsters.add(monster)
+
+        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
+            finish = True 
+            window.blit(lose, (200, 200))
 
 
+        if score >= goal:
+            finish = True
+            window.blit(win, (200, 200))
 
+        display.update()
 
-
-
-
-
-    display.update()
-    clock.tick(FPS)
+    time.delay(50)
